aec0fb96fe9a8ae5ae18ce53728fb45ec4059628
stharanzn <38763905+stharanzn@users.noreply.github.com>
2022-10-01 16:56:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# GUN Chat Dapp Demo
+
+A decentralized chat app built with [GUN](https://gun.eco/). 
+
+
+```
+git clone <this-repo>
+npm install
+npm run dev
+```
---
+++ src/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<!-- svelte-ignore a11y-missing-content -->
+<h1 class="logo"></h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Hello, <strong>{$username}</strong></span>
+      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
+    </div>
+
+    <button class="signout-button" on:click={signout}></button>
+
+    {:else}
+
+      <h1>💬</h1>
+  {/if}
+</header>
